e2c5a9144606dddb90b2c384347cc01c25c7390b
Juan Eduardo Tenreyro <tenreyrojuan1@gmail.com>
2026-09-05 16:44:34 -0300

    Añadir boton interactivo para alternar el navbar

--- dashboard.js
+++ dashboard.js
@@ -4,4 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   logoutButton.addEventListener('click', () => {
     window.location.href = 'login.html';
   });
+
+  const navButton = document.getElementById('menu-button');
+  const navbar = document.getElementById('sidebar');
+
+  navButton.addEventListener('click',() => {
+    navbar.classList.toggle('open');
+  });
 });
