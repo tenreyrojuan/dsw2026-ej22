19c2fe16dd8afa4d34ade11f7e19d004e600d6e9
Lucas <lucasalarcon1995@gmail.com>
2026-09-15 16:40:41 -0300

    Ejercicio 22

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const logoutButton = document.getElementById('logout');
-
   logoutButton.addEventListener('click', () => {
     window.location.href = 'login.html';
+
   });
+
+  const menuBtn = document.getElementById('menu');
+  const nav = document.getElementById('sidebar');
+
+  menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+
 });
