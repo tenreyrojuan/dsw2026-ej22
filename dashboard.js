94953233cdaa84ebec21f41681dc82b72e8d0af3
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-09-21 01:05:57 -0300

    implementación de la función de mostrar/ocultar menú

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const logoutButton = document.getElementById('logout');
+    const logoutButton = document.getElementById('logout');
 
-  logoutButton.addEventListener('click', () => {
-    window.location.href = 'login.html';
-  });
+    logoutButton.addEventListener('click', () => {
+        window.location.href = 'login.html';
+    });
+
+    const menuBtn = document.getElementById('menu');
+    const nav = document.getElementById('sidebar');
+
+    menuBtn.addEventListener('click', () => {
+        if (window.innerWidth < 600) {
+            nav.classList.toggle('open');
+        }
+    });
 });
